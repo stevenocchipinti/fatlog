668f94dd15089e9c9b99cebea93b1ddea6f448e7
Steve Occhipinti <dev@stevenocchipinti.com>
2017-02-10 23:53:16 +1100

    wip, but touch stuff and window resizes work

--- src/components/CheckinChart.js
+++ src/components/CheckinChart.js
@@ -1,65 +1,97 @@
-import React from "react";
-import { max, min } from "d3";
+import React, { Component } from "react";
+import { line } from "d3";
 import { scaleLinear, scaleTime } from "d3-scale";
-import Line from "./Line";
-import XAxis from "./XAxis";
+import { axisBottom } from "d3-axis";
+import { zoom } from "d3-zoom";
+import { select } from "d3-selection";
 import Paper from "material-ui/Paper";
-import { lightBlue500, orange500, green500 } from "material-ui/styles/colors";
 
-export default props => {
-  const margin = 10;
-  const width = props.width - margin * 2;
-  const verticalPadding = 20;
-  const horizontalPadding = 10;
+const MARGINS = { top: 20, right: 20, bottom: 20, left: 20 };
+const HEIGHT = 250 - MARGINS.left - MARGINS.right;
+const WIDTH = 250 - MARGINS.top - MARGINS.bottom;
 
-  const xScale = (data, accessor) => {
-    return scaleTime()
-      .domain([ min(data, accessor), max(data, accessor) ])
-      .range([ horizontalPadding, width - horizontalPadding ]);
-  };
+class CheckinChart extends Component {
+  constructor(props) {
+    super(props);
+    this.state = this.propsToState(props);
+  }
 
-  const yScale = (data, accessor) => {
-    return scaleLinear()
-      .domain([ min(data, accessor), max(data, accessor) ])
-      .range([ props.height - verticalPadding * 2, verticalPadding ]);
-  };
+  componentWillReceiveProps(props) {
+    this.setState(this.propsToState(props));
+  }
 
-  const date = d => new Date(d.date);
-  const dateScale = xScale(props.checkins, date);
+  propsToState(props) {
+    let dateScale = scaleTime()
+      .range([ 0, props.width - 20 - MARGINS.left - MARGINS.right ])
+      .domain([ new Date("2017-01-01"), new Date("2017-01-22") ]);
 
-  const fat = d => d.fat;
-  const fatScale = yScale(props.checkins, fat);
+    return {
+      innerWidth: props.width - 20,
+      dateScale: dateScale,
+      originalDateScale: dateScale.copy(),
+      yScale: scaleLinear().range([ props.height, 0 ]).domain([ 20, 26 ]),
+      data: [
+        { date: new Date("2017-01-01"), value: 25 },
+        { date: new Date("2017-01-08"), value: 23 },
+        { date: new Date("2017-01-15"), value: 24 },
+        { date: new Date("2017-01-22"), value: 21 }
+      ]
+    };
+  }
 
-  const weight = d => d.weight;
-  const weightScale = yScale(props.checkins, weight);
+  componentDidMount() {
+    const zoomBehaviour = zoom()
+      .scaleExtent([ 1, Infinity ])
+      .translateExtent([ [ 0, 0 ], [ this.state.innerWidth, HEIGHT ] ])
+      .extent([ [ 0, 0 ], [ this.state.innerWidth, HEIGHT ] ])
+      .on("zoom", () => {
+        const e = require("d3-selection").event;
+        this.setState({
+          ...this.state,
+          dateScale: e.transform.rescaleX(this.state.originalDateScale)
+        });
+      });
+    select(this.refs.focus).call(zoomBehaviour);
+  }
 
-  const waist = d => d.waist;
-  const waistScale = yScale(props.checkins, waist);
+  render() {
+    const xAxisRef = ref => {
+      select(ref).call(axisBottom(this.state.dateScale));
+    };
 
-  return (
-    <Paper style={{ margin }} zDepth={1}>
-      <svg width={width} height={props.height}>
-        <Line
-          x={d => dateScale(date(d))}
-          y={d => fatScale(fat(d))}
-          data={props.checkins}
-          color={lightBlue500}
-        />
-        <Line
-          x={d => dateScale(date(d))}
-          y={d => weightScale(weight(d))}
-          data={props.checkins}
-          color={green500}
-        />
-        <Line
-          x={d => dateScale(date(d))}
-          y={d => waistScale(waist(d))}
-          data={props.checkins}
-          color={orange500}
-        />
-        <XAxis position={props.height - verticalPadding} scale={dateScale} />
-      </svg>
-    </Paper>
-  );
-};
+    const lineGenerator = line()
+      .x(d => this.state.dateScale(d.date))
+      .y(d => this.state.yScale(d.value));
+    const linePath = lineGenerator(this.state.data);
+
+    return (
+      <Paper style={{ margin: 10 }} zDepth={1}>
+        <svg width={this.state.innerWidth} height={this.props.height}>
+          <defs>
+            <clipPath id="clip">
+              <rect width={this.state.innerWidth - 40} height={
+                this.props.height
+              } />
+            </clipPath>
+          </defs>
+          <g ref="focus" className="focus" transform={
+            `translate(${MARGINS.left}, ${MARGINS.top})`
+          }>
+            <g ref={xAxisRef} className="xAxis" transform={
+              `translate(0, ${HEIGHT})`
+            } />
+            <path className="line" style={
+              { fill: "none", stroke: "steelblue", clipPath: "url(#clip)" }
+            } d={linePath} />
+            <rect className="zoom" height={HEIGHT} width={WIDTH} style={
+              { cursor: "move", fill: "none", pointerEvents: "all" }
+            }></rect>
+          </g>
+        </svg>
+      </Paper>
+    );
+  }
+}
+
+export default CheckinChart;
 
