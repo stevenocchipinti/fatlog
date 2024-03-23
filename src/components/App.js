9da3cfb1c21794fc9b5ea89b500832320e505709
Steve Occhipinti <dev@stevenocchipinti.com>
2024-03-23 23:01:44 +1100

    A landing page to redirect users to the new URL
    See previous commit for details

--- src/components/App.js
+++ src/components/App.js
@@ -1,144 +1,30 @@
 import React, { Component } from "react";
+import { unregister } from "../registerServiceWorker";
 
-import backend from "../backend";
-import { loadState, saveState, clearState } from "../localStorage";
-import { registerServiceWorker } from "../registerServiceWorker";
-
-import Homepage from "./Homepage";
-import CheckinChart from "./CheckinChart";
-import CheckinTable from "./CheckinTable";
-import NewCheckinDialog from "./NewCheckinDialog";
-import AppBar from "./AppBar";
-import Footer from "./Footer";
-
-import Snackbar from "material-ui/Snackbar";
+unregister();
 
 class App extends Component {
-
-  constructor() {
-    super();
-    this.state = {
-      checkins: [],
-      notification: {
-        message: "",
-        visible: false
-      },
-      loading: true,
-      offline: !navigator.onLine,
-      viewportWidth: window.innerWidth
-    };
-  }
-
-  componentWillUpdate(props, state) {
-    saveState({
-      checkins: state.checkins,
-      user: state.user
-    });
-  }
-
-  componentDidMount() {
-    let persistedState = loadState();
-    if (persistedState) {
-      this.setState({
-        ...this.state,
-        checkins: persistedState.checkins,
-        user: persistedState.user
-      });
-    }
-
-    if (window) window.addEventListener("online", () => {
-      this.setState({...this.state, offline: false});
-    });
-    if (window) window.addEventListener("offline", () => {
-      this.setState({...this.state, offline: true});
-    });
-    if (window) window.addEventListener("resize", e => {
-      this.setState({...this.state, viewportWidth: e.target.innerWidth});
-    });
-
-    backend.init({
-      onAuthStateChanged: user => {
-        this.setState({...this.state, user});
-        if (user) this.installServiceWorker();
-      },
-      onCheckinsChanged: checkins => {
-        this.setState({
-          ...this.state,
-          loading: false,
-          checkins
-        });
-      }
-    });
-  }
-
-  installServiceWorker() {
-    registerServiceWorker({
-      onInstall: () => { this.notify("Now available offline"); },
-      onUpdate: () => { this.notify("Refresh for the new version"); }
-    });
-  }
-
-  handleCreate(checkin) {
-    backend.addCheckin(checkin).then(() => {
-      this.notify("Check-in created");
-    });
-  }
-
-  handleDelete(checkinKey) {
-    backend.deleteCheckin(checkinKey).then(() => {
-      this.notify("Check-in deleted");
-    });
-  }
-
-  notify(message) {
-    this.setState({
-      ...this.state,
-      notification: { message, visible: true }
-    });
-  }
-  hideNotification() {
-    this.setState({
-      ...this.state,
-      notification: { message: "", visible: false }
-    });
-  }
-
   render() {
-    if (!this.state.user) return <Homepage signIn={backend.signIn} />;
-
     return (
-      <div className="App">
-        <AppBar
-          onSignIn={ () => { backend.signIn(); } }
-          onSignOut={ () => { backend.signOut(); clearState(); } }
-          user={this.state.user}
-          loading={this.state.user && this.state.loading}
-          offline={this.state.offline}
+      <main>
+        <h1>FatLog</h1>
+        <img
+          width={150}
+          height={150}
+          src="/android-chrome-192x192.png"
+          alt="Fatlog logo"
         />
-
-        <CheckinChart
-          checkins={ this.state.checkins }
-          viewportWidth={ this.state.viewportWidth }
-          height={250}
-        />
-
-        <CheckinTable
-          checkins={ this.state.checkins }
-          onDelete={ (key) => this.handleDelete(key) }
-        />
-
-        <NewCheckinDialog onSubmit={ checkin => this.handleCreate(checkin) }/>
-
-        <Footer />
-
-        <Snackbar
-          open={this.state.notification.visible}
-          message={this.state.notification.message}
-          style={{textAlign: "center"}}
-          autoHideDuration={3000}
-          onRequestClose={ () => { this.hideNotification(); } }
-        />
-      </div>
+        <p>
+          This app has moved to a new location.
+          <br />
+          <br />
+          Please update your bookmark to use{" "}
+          <a href="https://fatlog.web.app">fatlog.web.app</a> instead.
+        </p>
+        <a className="link-button" href="https://fatlog.web.app">
+          Goto new URL
+        </a>
+      </main>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { initializeApp } from "firebase";
 import injectTapEventPlugin from "react-tap-event-plugin";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import App from "./components/App";
@@ -8,19 +7,11 @@ import getMuiTheme from "material-ui/styles/getMuiTheme";
 import theme from "./theme";
 import "./index.css";
 
-initializeApp({
-  apiKey: "AIzaSyCk78H-sLkJt6WbwqM9tAP9DIeZe9iWOTA",
-  authDomain: "fatlog-app.firebaseapp.com",
-  databaseURL: "https://fatlog-app.firebaseio.com",
-  storageBucket: "fatlog-app.appspot.com",
-  messagingSenderId: "893039177685"
-});
-
 injectTapEventPlugin();
 
 ReactDOM.render(
   <MuiThemeProvider muiTheme={getMuiTheme(theme)}>
     <App />
   </MuiThemeProvider>,
-  document.getElementById("root")
+  document.getElementById("root"),
 );
